d59cfb4c0708a149e2581b186cd01fe65a4d4ead
mazhar_laptop <mazhar@MAZHAR-PC>
2022-12-24 20:34:00 +0530

    End of stage 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,36 +1,43 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetail from "../../features/catalog/ProductDetail";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Hearder";
-
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const palletType = darkMode ? 'dark' : 'light'
+  const palletType = darkMode ? "dark" : "light";
   const theme = createTheme({
     palette: {
       mode: palletType,
       background: {
-        default: palletType === 'light' ? '#eaeaea' : '#121212'
-      }
-    }
+        default: palletType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
   });
- 
-function handleThemeChange(){
-  setDarkMode(!darkMode);
-}
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
-    
     <ThemeProvider theme={theme}>
-      
-      <CssBaseline/>
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetail />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </Container>
-      
     </ThemeProvider>
   );
 }

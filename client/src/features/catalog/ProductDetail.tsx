1e1b309e786baa35e1125129928209ecc8a88f73
mazhar_laptop <mazhar@MAZHAR-PC>
2023-01-01 16:21:50 +0530

    Error Handling in Code Part 1

--- client/src/features/catalog/ProductDetail.tsx
+++ client/src/features/catalog/ProductDetail.tsx
@@ -8,21 +8,20 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetail() {
-  const { id } = useParams<{ id: string }>();
+  const {id} = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/products/${id}`)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.log(error))
+    agent.Catalog.details(parseInt(id!))
+      .then((response) => setProduct(response))
+      .catch((error) => console.log(error.response))
       .finally(() => setLoading(false));
   }, [id]);
 
